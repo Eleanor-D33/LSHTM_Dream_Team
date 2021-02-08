b555a5d877919c77301e0d86dc90213ddf6c26f2
Eleanor-D33 <40007262+Eleanor-D33@users.noreply.github.com>
2021-02-08 18:25:15 +0000

    Update csv merger.R

--- csv merger.R
+++ csv merger.R
@@ -9,21 +9,5 @@ files <- list.files(path = wd,
 
 as.data.frame(files)
 
-write.csv(files,  file = "/Users/eleanordavies/Desktop/HDS_Content/Term_2/Data_Challenge/Tweet_Data/Meltwater/Anxiety/meltwater_anxiety.csv")
-
+# reading in UTF-16 docs 
 df <-read.csv("/Users/eleanordavies/Desktop/csv/Anxiety_2021-02-06-12-47-25.csv", encoding="UTF-16", stringsAsFactors=FALSE)
-
-
-csv_file <- "/Users/eleanordavies/Desktop/csv/Anxiety_2021-02-06-12-47-25.csv"
-
-
-read.csv(
-  csv_file, skip = 1, fileEncoding = "UTF-16", sep = "\t", header = FALSE
-)
-wd1 <- "/Users/eleanordavies/Desktop/csv"
-files1 <- list.files(path = wd,
-                              pattern="*.csv", 
-                              full.names = T) %>% 
-  map_df(~read_csv(., skip = 1, fileEncoding = "UTF-16", sep = "\t", header = FALSE))
-
-as.data.frame(files1)
